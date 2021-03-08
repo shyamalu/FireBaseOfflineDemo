2e8d7ce20d5bd7b42d2b7e559e4418a232c24c16
shyamalu <shyamal@amiti.in>
2021-03-08 19:55:28 +0530

    integration with Room

--- app/src/main/java/org/chimple/myapplication/MainActivity.java
+++ app/src/main/java/org/chimple/myapplication/MainActivity.java
@@ -3,42 +3,15 @@ package org.chimple.myapplication;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
-import com.google.firebase.firestore.ListenerRegistration;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 import org.chimple.myapplication.database.AppDatabase;
-import org.chimple.myapplication.database.AppExecutors;
 import org.chimple.myapplication.database.DbOperations;
 import org.chimple.myapplication.database.FirebaseOperations;
-import org.chimple.myapplication.model.School;
-import org.chimple.myapplication.model.Section;
-import org.chimple.myapplication.model.Student;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = MainActivity.class.getSimpleName();
-
-    private static MainActivity activityRef = null;
 
     private AppDatabase mDb;
     private FirebaseOperations firebaseOperations;
@@ -47,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MainActivity.activityRef = this;
-
         mDb = AppDatabase.getInstance(getApplicationContext());
         firebaseOperations = FirebaseOperations.getInstance(DbOperations.getInstance(mDb));
     }
